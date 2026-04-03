1f1d4f3c61d2c29f3604c987c7ca083e74bb3850
PLGQ64 <pedrolucas.q64@gmail.com>
2026-04-03 18:54:46 -0300

    Add files via upload

---
+++ Vetores e matrizes/02.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+  int linhas, colunas;
+  // atribui o valor para linhas
+  printf("numero de linhas: ");
+  scanf(" %d", &linhas);
+  // o getchar por segurança consome o caractere que ainda esteja no buffer
+  getchar();
+  // atribui o valor para colunas
+  printf("numero de colunas: ");
+  scanf(" %d", &colunas);
+  getchar();
+
+  // a primeira coluna deve ser alocada com o tamanho do total de linhas
+  // para que ela possa armazenar os ponteiros que serao as linas
+  int **matriz = (int**)malloc(linhas * sizeof(int*));
+  // verifica se a alocaçao ocorreu com sucesso
+  if(!matriz){printf("erro de alocacao"); exit(0);}
+
+  // agora as linhas sao alocadas
+  // o for percorre a primera coluna alocando um ponteiro que sera a linha
+  // e isso é feito para cada indice da primeira coluna
+  for(int i = 0; i < linhas; i++){
+    matriz[i] = (int*)malloc(colunas * sizeof(int));
+    if(!matriz[i]){printf("erro de alocacao da linha"); exit(0);}
+  }
+
+
+  // a mesmo que o snterior so que agora para a matriz transposta
+  // com os valores trocados porque é uma matriz transposta
+  int **tmatriz = (int**)malloc(colunas * sizeof(int*));
+  if(!tmatriz){printf("erro de alocacao da tmatriz"); exit(0);}
+
+  for(int i = 0; i < colunas; i++){
+    tmatriz[i] = (int*)malloc(linhas * sizeof(int));
+    if(!tmatriz[i]){printf("erro de alocacao da linha da tmatriz"); exit(0);}
+  }
+
+
+  // os valores sao inseridos pelo usuario na matriz
+  for(int i = 0; i < linhas; i++){
+    for(int j = 0; j < colunas; j++){
+      printf("valor da matris (%d, %d): ",i,j);
+      scanf("%d", &matriz[i][j]);
+    }
+  }
+  // a matriz transposta e criada
+  for(int i = 0; i < linhas; i++){
+    for(int j = 0; j < colunas; j++){
+      tmatriz[j][i] = matriz[i][j];
+    }
+  }
+
+  // a matriz inserida e impressa
+  printf("matriz inserida: \n");
+  for(int i = 0; i < linhas; i++){
+    for(int j = 0; j < colunas; j++){
+      printf("%d",matriz[i][j]);
+    }
+    printf("\n");
+  }
+
+  // a matriz transposta e impresa
+  printf("matriz transposta: \n");
+  for(int i = 0; i < colunas; i++){
+    for(int j = 0; j < linhas; j++){
+      printf("%d",tmatriz[i][j]);
+    }
+    printf("\n");
+  }
+
+  // agora para liberar a memoria deve ser liberada de dentro para fora
+  // porque se a primeira coluna que foi criada for liberada
+  // nao teremos mais como liberar as linhas pois nao teremos accesso 
+  // ao endereço de memoria no qual elas se encontram
+  for (int i = 0; i < linhas; i++) free(matriz[i]);
+    free(matriz);
+  for (int i = 0; i < colunas; i++) free(tmatriz[i]);
+    free(tmatriz);
+
+  return 0;
+}
---
+++ Vetores e matrizes/04.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+  int quantidade_pesoas;
+  // o ponteiro vazio é criado que sera usado como um vetor
+  int *v; 
+  // um ponteiro de ponteiro é criado vazio e sera usado como uma matriz
+  char **matriz;
+
+  printf("quantidade de pessoas: ");
+  scanf("%d", &quantidade_pesoas);
+  getchar();
+
+  // a coluna inicial da matriz é alocada normalmente 
+  matriz = (char**)malloc(quantidade_pesoas * sizeof(char*));
+  if(!matriz){printf("null"); exit(1);}
+  // a linhas que foram alocads nas questoes anteriores erao vetores
+  // agora elas sao usadas para armazenar uma string que é um vetor de chars
+  for(int i = 0; i < quantidade_pesoas; i++){
+    matriz[i] = (char*)malloc(128 * sizeof(char));
+    if(!matriz[i]){printf("null [%d]", i); exit(1);}
+  }
+
+  // esse vetor que foi criado anteriormente
+  // agora e allocado dinamicamente com (quantidade_pessoas) de elementos 
+  v = (int*)malloc(quantidade_pesoas * sizeof(int));
+
+  for(int i = 0; i < quantidade_pesoas; i++){
+    // o nome é alocado em uma na linha i da matriz
+    printf("nome [%d]: ",i);
+    scanf("%[^\n]s", &matriz[i][0]);
+    getchar();
+    // é a idade é alocada no indice i do vetor
+    printf("idade [%d]: ",i);
+    scanf("%d", (v+i));
+    getchar();
+  }
+
+  // o nome e idade das pessoa e imprimidido
+  for(int i = 0; i < quantidade_pesoas; i++){
+    printf("nome completo: %s; idade: %d\n", matriz[i], v[i]);
+  }
+
+  // memoria e liberada, mesma logica das questoes anterior
+  // com excesao do vetor porque ele nao é um vetor de ponteiros
+  // entao ele so precisa de um free
+  for(int i = 0; i < quantidade_pesoas; i++){free(matriz[i]);}
+  free(matriz);
+  free(v);
+
+  return 0;
+}
